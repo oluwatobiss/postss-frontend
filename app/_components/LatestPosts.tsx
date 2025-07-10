230b4bace15787af902a6a4c388d3ee064b19bc4
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2025-07-10 10:29:54 +0100

    feat: Create Aside component

--- app/_components/LatestPosts.tsx
+++ app/_components/LatestPosts.tsx
@@ -1,27 +1,10 @@
-import { Mr_Bedfort } from "next/font/google";
+import Aside from "./Aside";
 import { svg } from "../_svg";
-
-const mrBedfort = Mr_Bedfort({ weight: "400", subsets: ["latin"] });
 
 export default function LatestPosts() {
   return (
     <article>
-      <aside className="fixed bg-[rgba(10, 10, 10, 0.85)] backdrop-blur-lg h-full w-19 flex flex-col items-center">
-        <div
-          className={`${mrBedfort.className} text-[33px] hover:scale-110 cursor-pointer transition-scale duration-300 linear leading-[1.5] py-4`}
-        >
-          P
-        </div>
-        <div className="grow flex items-center">
-          <div className="grid gap-y-4 [&_div]:size-15 [&_div]:rounded-xl [&_div]:flex [&_div]:items-center [&_div]:justify-center [&_div]:cursor-pointer [&_div]:hover:bg-[rgba(255,255,255,0.08)] [&_svg]:text-[rgb(77,77,77)]">
-            <div>{svg.person}</div>
-            <div className="bg-[rgba(255,255,255,0.08)] group [&_svg]:group-hover:text-white">
-              {svg.plus}
-            </div>
-            <div>{svg.boxArrowRight}</div>
-          </div>
-        </div>
-      </aside>
+      <Aside />
       <main>
         <h1>Latest</h1>
         <div>
