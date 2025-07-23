5bfe29d6443d7562864c6fbe7ff02ae2a860c68d
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2025-07-23 20:38:11 +0100

    refactor: Enable allowShortCircuit and allowTernary linting options

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,12 @@ const eslintConfig = [
   ...compat.config({
     extends: ["next/core-web-vitals", "next/typescript"],
     rules: {
-      "@typescript-eslint/no-unused-expressions": "off",
+      "@typescript-eslint/no-unused-expressions": [
+        {
+          allowShortCircuit: true,
+          allowTernary: true,
+        },
+      ],
     },
   }),
 ];
