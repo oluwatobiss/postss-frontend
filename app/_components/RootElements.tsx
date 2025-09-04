ba2a580621db6913ac0f4fe56a50a6e1c7837210
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2025-09-04 13:35:58 +0100

    chore: Specify each page's heading

--- app/_components/RootElements.tsx
+++ app/_components/RootElements.tsx
@@ -29,7 +29,16 @@ export default function RootElements({ children }: ChildrenProps) {
   });
   const dialogRef = useRef<HTMLDialogElement | null>(null);
   const pathname = usePathname();
-  const heading = pathname === "/" ? "Latest" : "Profile";
+
+  function getHeading(pathname: string) {
+    if (pathname === "/") return "Latest";
+    if (pathname.includes("/edit-profile")) return "Edit Profile";
+    if (pathname.includes("/login")) return "Login";
+    if (pathname.includes("/post/")) return "Post";
+    if (pathname.includes("/profile/")) return "Profile";
+    if (pathname.includes("/sign-up")) return "Sign up";
+    return "SUPER SECTION";
+  }
 
   function openPostDialog(postInfo: { isNewPost: boolean; post: PostProps }) {
     setPostInfo(postInfo);
@@ -49,7 +58,9 @@ export default function RootElements({ children }: ChildrenProps) {
               <PostsContextProvider>
                 <CommentsContextProvider>
                   <main className="w-full fixed min-h-screen">
-                    <h1 className="text-center h-15 leading-15">{heading}</h1>
+                    <h1 className="text-center h-15 leading-15">
+                      {getHeading(pathname)}
+                    </h1>
                     <div className="w-full fixed top-15 bottom-0 overflow-y-scroll">
                       <div className="w-160 mx-auto bg-[#181818] border-[.5px] border-[rgb(45,45,45)] py-3 cursor-pointer shadow-[0_0_12px_0_rgba(0,0,0,0.04)]">
                         {children}
