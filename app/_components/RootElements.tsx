6aa70e06564949cef6a54bc2bd3bbe4afb39f6e9
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2025-09-17 19:34:49 +0100

    chore: Show header in mobile screens

--- app/_components/RootElements.tsx
+++ app/_components/RootElements.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useRef, useState } from "react";
 import { usePathname } from "next/navigation";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Mr_Bedfort } from "next/font/google";
 import { ChildrenProps, PostProps } from "@/app/_types";
 import { PostDialogContext } from "./context/Contexts";
 import { CommentsContextProvider } from "./context/CommentsContextProvider";
@@ -11,12 +11,13 @@ import { UserTokenNDataContextProvider } from "./context/UserTokenNDataContextPr
 import { defaultPost } from "../_defaultContexts";
 import Aside from "./Aside";
 import PostDialog from "./PostDialog";
+import Link from "next/link";
 
+const mrBedfort = Mr_Bedfort({ weight: "400", subsets: ["latin"] });
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
-
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
@@ -59,9 +60,22 @@ export default function RootElements({ children }: ChildrenProps) {
               <PostsContextProvider>
                 <CommentsContextProvider>
                   <main className="w-full fixed min-h-screen">
-                    <h1 className="text-center h-15 leading-15">
+                    <h1 className="hidden md:block text-center h-15 leading-15">
                       {getHeading(pathname)}
                     </h1>
+                    <header className="md:hidden flex justify-between items-center mx-4">
+                      <Link href="/">
+                        <div
+                          className={`${mrBedfort.className} text-[33px] hover:scale-110 cursor-pointer transition-scale duration-300 linear leading-[1.5]`}
+                        >
+                          P
+                        </div>
+                      </Link>
+                      <h1 className="text-center h-15 leading-15">
+                        {getHeading(pathname)}
+                      </h1>
+                      <span>menu</span>
+                    </header>
                     <div className="w-full fixed top-15 bottom-0 overflow-y-scroll">
                       <div className="md:w-160 mx-auto bg-[#181818] border-[.5px] border-[rgb(45,45,45)] py-3 cursor-pointer shadow-[0_0_12px_0_rgba(0,0,0,0.04)]">
                         {children}
